0ea69f3c76317a75c515717fcdc90dbb5d0bcb78
lmb <1603497362@qq.com>
2018-11-07 14:32:01 +0800

    Add project files.

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,134 @@
+﻿
+namespace AddTwoNumbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode resultNode = new ListNode(0);
+            ListNode currentNode = resultNode;
+            ListNode next1 = l1;
+            ListNode next2 = l2;
+            bool sum10 = false;
+
+            do
+            {
+
+                int val1 = (next1 == null ? 0 : next1.val);
+                int val2 = (next2 == null ? 0 : next2.val);
+
+                int sum = 0;
+                if (sum10)
+                {
+                    sum = val1 + val2 + 1;
+                }
+                else
+                {
+                    sum = val1 + val2;
+                }
+
+
+                if (sum >= 10)
+                {
+                    currentNode.val += (sum - 10);
+                    sum10 = true;
+                }
+                else
+                {
+                    currentNode.val += sum;
+                    sum10 = false;
+                }
+
+
+
+                if (next1 == null && next2 == null)
+                {
+                    if (sum10 == false)
+                    {
+                        currentNode.next = null;
+                    }
+                    else
+                    {
+                        currentNode.next = new ListNode(0);
+                    }
+
+                }
+                if (next1 != null && next2 == null)
+                {
+                    if (next1.next == null && sum10 == false)
+                    {
+                        currentNode.next = null;
+                    }
+                    else
+                    {
+                        currentNode.next = new ListNode(0);
+                    }
+
+                }
+                if (next1 == null && next2 != null)
+                {
+                    if (next2.next == null && sum10 == false)
+                    {
+                        currentNode.next = null;
+                    }
+                    else
+                    {
+                        currentNode.next = new ListNode(0);
+                    }
+
+                }
+                if (next1 != null && next2 != null)
+                {
+                    if ((next1.next == null) && (next2.next == null) && (sum10 == false))
+                    {
+                        currentNode.next = null;
+                    }
+                    else
+                    {
+                        currentNode.next = new ListNode(0);
+                    }
+
+                }
+
+
+
+
+
+
+
+                currentNode = currentNode.next;
+                if (next1 != null)
+                {
+                    next1 = next1.next;
+                }
+                else
+                {
+                    next1 = null;
+                };
+
+                if (next2 != null)
+                {
+                    next2 = next2.next;
+                }
+                else
+                {
+                    next2 = null;
+                }
+
+
+
+
+            } while (currentNode != null || (sum10 == true));
+            return resultNode;
+
+
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddTwoNumbers;
+
+namespace Tests
+{
+    [TestClass]
+    public class AddTwoNumbersTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //[2,4,3]
+            //[5,6,4]
+            var result = AddTwoNumbers.Solution.AddTwoNumbers(
+                    new ListNode(2)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(3)
+                            {
+                                next = null
+                            }
+                        }
+                    },
+                    new ListNode(5)
+                    {
+                        next = new ListNode(6)
+                        {
+                            next = new ListNode(4)
+                            {
+                                next = null
+                            }
+                        }
+                    });
+
+            Assert.AreEqual(result.val, 7);
+            Assert.AreEqual(result.next.val, 0);
+            Assert.AreEqual(result.next.next.val, 8);
+            Assert.AreEqual(result.next.next.next, null);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //[1]
+            //[9,9]
+            var result = AddTwoNumbers.Solution.AddTwoNumbers(
+                    new ListNode(1)
+                    {
+                        next = null
+                    },
+                    new ListNode(9)
+                    {
+                        next = new ListNode(9)
+                        {
+                            next = null
+                        }
+                    });
+
+            Assert.AreEqual(result.val, 0);
+            Assert.AreEqual(result.next.val, 0);
+            Assert.AreEqual(result.next.next.val,1);
+            Assert.AreEqual(result.next.next.next, null);
+
+        }
+    }
+}
+
